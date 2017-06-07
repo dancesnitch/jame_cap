d22f85ab5ca2a59136469d86f89ff7ec8459ef3a
Daniel Byrne <byrne.daniel87@gmail.com>
2017-06-07 14:33:11 +0100

    manual Js to asset pipeline

--- public/assets/application-7ba0c200a6d5a548ce389b7c96ca8f3d2b79c4bd6c4e409083adac816f60939a.js
+++ public/assets/application-7ba0c200a6d5a548ce389b7c96ca8f3d2b79c4bd6c4e409083adac816f60939a.js
@@ -11,6 +11,38 @@
  *
  * Date: 2016-05-20T17:17Z
  */
+ (function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery); // End of use strict
+
 
 
 (function( global, factory ) {
